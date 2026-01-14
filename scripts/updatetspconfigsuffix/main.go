dd248f390ec6cc68f36fbce978a0d8bdb49ecd73
Judy Liu <v-liujudy@microsoft.com>
2026-01-14 17:42:52 +0800

    add test code

--- scripts/updatetspconfigsuffix/main.go
+++ scripts/updatetspconfigsuffix/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"updatetspconfigsuffix/model"
 
@@ -27,7 +28,8 @@ func main() {
 			return err // stop walking if error
 		}
 
-		// Check for exact match
+		// Check for spec match
+		/*
 		if d.IsDir() && fileExists(path+"\\"+"tsp-location.yaml") && strings.Contains(path, "\\arm") && !strings.Contains(path, "fake") && !strings.Contains(path, "node_modules") {
 			if strings.Contains(content, path) {
 				// fmt.Println(path, " has been generated")
@@ -58,6 +60,48 @@ func main() {
 			}
 			newModule := strings.Join(arr2, "/")
 			fmt.Println("    Updated module to:", newModule)
+			return nil
+		}*/
+
+		// Check for autorest match
+		if d.IsDir() && fileExists(path+"\\"+"autorest.md") && strings.Contains(path, "\\arm") && !strings.Contains(path, "fake") && !strings.Contains(path, "node_modules") {
+			if strings.Contains(content, path) {
+				// fmt.Println(path, " has been generated")
+				return nil
+			}
+			firstLine := getGoModFirstLine(path)
+			arr := strings.Split(firstLine, "/")
+			suffix := arr[len(arr)-1]
+			lastChar := suffix[len(suffix)-1]
+			if !isNumber(lastChar) {
+				return nil
+			}
+			configFile := getAutorestConfigFilePath(path)
+			if configFile == "" {
+				fmt.Println("firstLine:", firstLine)
+				return nil
+			}
+			configFiledep := "D:/GoProject2/azure-rest-api-specs" + "/" + configFile + "readme.go.md"
+			// fmt.Println(configFiledep, "===================", configFile)
+			if !fileExists(configFiledep) {
+				fmt.Println("Folder changed===================", configFiledep, "suffix:", suffix)
+				return nil
+			}
+			module, err := getReadmeGoPackageModule(configFiledep)
+			if err != nil {
+				fmt.Println("Error getting package module:", err, "configfile path :", configFiledep)
+				return nil
+			} 
+
+			arr2 := strings.Split(module, "/")
+			if isNumber(arr2[len(arr2)-1][len(arr2[len(arr2)-1])-1]) {
+				fmt.Println("old module is-----:", strings.Join(arr2, "/"))
+				arr2[len(arr2)-1] = suffix
+			} else {
+				arr2 = append(arr2, suffix)
+			}
+			newModule := strings.Join(arr2, "/")
+			fmt.Println("    Updated module to-----:", newModule)
 		}
 		return nil
 	})
@@ -90,6 +134,48 @@ func getConfigFilePath(path string) string {
 	return result["directory"].(string) // return the value for the specified key
 
 }
+
+func getAutorestConfigFilePath(path string) string {
+	filePath := path + "/autorest.md" // replace with your file path
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+		return ""
+	}
+	// Regex to extract path after 'blob/<commit>/'
+	// re := regexp.MustCompile(`github\.com/.+?/blob/[^/]+/(specification/[^ ]+)`)
+	re := regexp.MustCompile(`specification/[^/]+/resource-manager/readme\.go\.md`)
+	matches := re.FindAllStringSubmatch(string(data), -1)
+
+	if len(matches) == 0 {
+		// fmt.Println("No specification paths found")
+		return ""
+	}
+	s := strings.Replace(matches[0][0], "readme.go.md", "", 1)
+	return s
+}
+
+func getReadmeGoPackageModule(path string) (string, error) {
+	// Read the markdown file
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	// Regex to capture module-name value
+	re := regexp.MustCompile(`(?m)^\s*module-name:\s*([^\s]+)\s*$`)
+	matches := re.FindSubmatch(data)
+
+	if len(matches) < 2 {
+		// fmt.Println("module-name not found")
+		return "", fmt.Errorf("module-name not found")
+	}
+
+	moduleName := string(matches[1])
+	// fmt.Println("module-name:", moduleName)
+	return moduleName, nil
+}
+
 
 func getPackageModule(path string) (string, error) {
 	data, err := os.ReadFile(path)
